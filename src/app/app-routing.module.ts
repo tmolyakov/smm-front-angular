31e74412298d6e7b69e97012796f8df17f9987e5
tmolyakov <tmolyakov@gmail.com>
2022-01-18 00:28:37 +0300

    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthComponent } from './pages/auth/auth.component';
+import {HomeComponent} from "./pages/home/home.component";
+import {RegisterComponent} from "./pages/register/register.component";
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent, data: { 'pageTitle': 'Login Form' } }
+  { path: '', component: HomeComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { AuthComponent } from './pages/auth/auth.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
-    // AuthComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    HomeComponent,
+    RegisterComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
